04ec61b3a4b33f25d2d1593462552c3e32084b15
Jackson Chandler <jackson.c.chandler@gmail.com>
2019-02-15 12:58:55 -0600

    HashTable

---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("001", ".Net");
+            ht.Add("002", "C#");
+            ht.Add("003", "ASP.Net");
+
+            ICollection key = ht.Keys;
+
+            Console.WriteLine(ht.ContainsKey("001"));
+            Console.WriteLine(ht.ContainsValue("C#"));
+            foreach (String k in key)
+            {
+                Console.WriteLine(ht[k]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
