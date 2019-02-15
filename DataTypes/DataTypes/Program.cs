51b3827457439dae548659a496df8350ac60fb44
Jackson Chandler <jackson.c.chandler@gmail.com>
2019-02-15 12:47:20 -0600

    Data Types

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 30;
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
