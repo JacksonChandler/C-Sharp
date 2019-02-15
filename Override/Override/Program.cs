9922dac845a7ee412c8a20339a23a8cbe98c3131
Jackson Chandler <jackson.c.chandler@gmail.com>
2019-02-15 16:19:56 -0600

    Projects
    Calendars and Palindrones

---
+++ Override/Override/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Override
+{
+    public class TextInput
+    {
+        string value;
+        public void Add(char c)
+        {
+            value += c;
+        }
+
+        public string GetValue()
+        {
+
+            return value.ToString();
+        }
+    }
+
+    public class NumericInput : TextInput
+    {
+        string value;
+        public new void Add(char c)
+        {
+            if (char.IsDigit(c))
+            {
+                value += c;
+            }
+        }
+    }
+
+    public class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        int i = 0;
+        int j = word.Length - 1;
+        word = word.ToLower();
+        while (i < j)
+        {
+            if (word[i] != word[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+    }
+
+}
